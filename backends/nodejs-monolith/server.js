e80fe4dab0b0943efa0f6ad7b39753c1bbec7c18
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2020-01-14 16:51:00 +0100

    Structuring route codes a bit.

--- backends/nodejs-monolith/server.js
+++ backends/nodejs-monolith/server.js
@@ -2,7 +2,6 @@ const esClient   = require('node-eventstore-client')
 const Koa        = require('koa')
 const koaBody    = require('koa-body')
 const koaJwt     = require('koa-jwt')
-const koaRouter  = require('koa-router')
 const koaSession = require('koa-session2')
 const cors       = require('@koa/cors')
 
@@ -15,29 +14,21 @@ const esEndpoint        = process.env.ES_ENDPOINT || config.eventstore || 'tcp:/
 const port              = process.env.PORT || 3000
 
 const main = async () => {
-  console.log('setting up')
   const app = new Koa()
   app.use(koaBody({ jsonLimit: '1kb' }))
   app.use(cors({ credentials: true, exposeHeaders: ['content-type'] }))
   app.use(koaJwt({ cookie: 'accessToken', secret: accessTokenSecret, passthrough: true }))
   app.use(koaSession())
 
-  const router = koaRouter()
-
   const es = esClient.createConnection({}, esEndpoint)
-  console.log('connecting es')
   es.connect()
-  console.log('connection initiated')
-  await new Promise(resolve => es.once('connected', resolve))
-  console.log('connected es')
+  await new Promise(resolve => es.once('connected', resolve)) // Add error message.
 
   app.use(await authRoutes({ accessTokenSecret, config }))
   app.use(await commentRoutes({ es }))
 
-  app.use(router.routes())
-
   app.listen({ port, host: '0.0.0.0' })
-  console.log('Listening')
+  console.log(`Listening on ${port}`)
 }
 
 main()
